e8289e33737d0b0ccb1c36f3d4863e7a70b025ff
Earl Chew <earl_chew@yahoo.com>
2021-08-08 09:35:52 -0700

    [respawn] Use library.a
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

--- respawn.c
+++ respawn.c
@@ -16,54 +16,24 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define _GNU_SOURCE
+#include "clk.h"
+#include "err.h"
+#include "fd.h"
+#include "int.h"
+#include "proc.h"
+#include "sig.h"
+#include "macros.h"
 
 #include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <getopt.h>
-#include <limits.h>
-#include <signal.h>
-#include <string.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/wait.h>
 
 /******************************************************************************/
-#define NUMBEROF(Array) ( sizeof((Array))/sizeof((Array)[0]) )
-
-/******************************************************************************/
-#define DEBUG(...) if (!optDebug) { } else debug(__VA_ARGS__)
-
-/******************************************************************************/
-#define FINALLY(...)                 \
-    do {                             \
-        int errno_ = errno;          \
-        do { __VA_ARGS__ } while(0); \
-        errno = errno_;              \
-    } while (0)
-
-/******************************************************************************/
-#ifdef __GNUC__
-#define PRINTF_FORMAT(Fmt, Args) \
-    __attribute__((__format__ (__printf__, Fmt, Args)))
-#else
-#define PRINTF_FORMAT(Fmt, Args)
-#endif
-
-/******************************************************************************/
-#ifdef __GNU_LIBRARY__
-#define ARGV0 (program_invocation_short_name)
-#else
-#define ARGV0 (getprogname())
-#endif
-
-/******************************************************************************/
-static int optDebug;
 static int optHelp;
 static int optContinue;
 static int optForever;
@@ -75,389 +45,21 @@ void
 usage(void)
 {
     static const char usageText[] =
-        "usage: %s [-dfZ] [-x N,...] -- cmd ...\n"
+        "[-dfZ] [-x N,...] -- cmd ...\n"
+        "\n"
         "Options:\n"
         "  -d --debug      Emit debug information\n"
         "  -f --forever    Continually restart the monitored process\n"
         "  -Z --continue   Continue monitored process if it suspends\n"
         "  -x --exit N,..  Additional success exit codes [default: 0]\n"
-        "  -x --exit none  No success exit codes [default: 0]\n";
+        "  -x --exit none  No success exit codes [default: 0]\n"
+        "\n"
+        "Arguments:\n"
+        "  cmd ...         Program to monitor\n";
 
-    if (optHelp)
-        fprintf(stderr, usageText, ARGV0);
-    else {
-        const char *usageLine = strchr(usageText, '\n');
-
-        if (!usageLine)
-            usageLine = strchr(usageText, 0);
-
-        size_t helpLen = usageLine - usageText;
-
-        char helpText[helpLen+2];
-
-        memcpy(helpText, usageText, helpLen);
-        helpText[helpLen+0] = '\n';
-        helpText[helpLen+1] = 0;
-
-        fprintf(stderr, helpText, ARGV0);
-    }
+    help(usageText, optHelp);
 
     exit(EXIT_FAILURE);
-}
-
-/******************************************************************************/
-static void
-alert_(const char *aFmt, const char *aLevel, int errCode, va_list argp)
-{
-    fprintf(stderr, "%s: ", ARGV0);
-    if (aLevel)
-        fprintf(stderr, "%s - ", aLevel);
-    vfprintf(stderr, aFmt, argp);
-
-    if (errCode) {
-        const char *errText = strerror(errCode);
-
-        fprintf(stderr, " [%d - %s]", errCode, errText);
-    }
-
-    fprintf(stderr, "\n");
-}
-
-/*----------------------------------------------------------------------------*/
-PRINTF_FORMAT(1, 2)
-void
-warn(const char *aFmt, ...)
-{
-    int errCode = errno;
-
-    va_list argp;
-
-    va_start(argp, aFmt);
-    alert_(aFmt, "WARN", errCode, argp);
-    va_end(argp);
-
-    errno = errCode;
-}
-
-/*----------------------------------------------------------------------------*/
-void
-errorv(const char *aFmt, va_list argp)
-{
-    int errCode = errno;
-
-    alert_(aFmt, "ERROR", errCode, argp);
-
-    errno = errCode;
-}
-
-/*----------------------------------------------------------------------------*/
-PRINTF_FORMAT(1, 2)
-void
-error(const char *aFmt, ...)
-{
-    va_list argp;
-
-    va_start(argp, aFmt);
-    errorv(aFmt, argp);
-    va_end(argp);
-}
-
-/*----------------------------------------------------------------------------*/
-PRINTF_FORMAT(1, 2)
-void
-fatal(const char *aFmt, ...)
-{
-    va_list argp;
-
-    va_start(argp, aFmt);
-    errorv(aFmt, argp);
-    va_end(argp);
-    exit(EXIT_FAILURE);
-}
-
-/*----------------------------------------------------------------------------*/
-PRINTF_FORMAT(1, 2)
-void
-debug(const char *aFmt, ...)
-{
-    va_list argp;
-
-    va_start(argp, aFmt);
-    alert_(aFmt, "DEBUG", 0, argp);
-    va_end(argp);
-}
-
-/*----------------------------------------------------------------------------*/
-PRINTF_FORMAT(1, 2)
-void
-die(const char *aFmt, ...)
-{
-    va_list argp;
-
-    va_start(argp, aFmt);
-    alert_(aFmt, 0, 0, argp);
-    va_end(argp);
-
-    exit(EXIT_FAILURE);
-}
-
-/******************************************************************************/
-unsigned long
-monomillis(void)
-{
-    struct timespec clockTime;
-
-    if (clock_gettime(CLOCK_MONOTONIC, &clockTime))
-        fatal("Unable to get CLOCK_MONOTONIC time");
-
-    return 1000 * clockTime.tv_sec + clockTime.tv_nsec / 1000000;
-}
-
-/******************************************************************************/
-int
-fd_cloexec(int aFd)
-{
-    int rc = -1;
-
-    int arg;
-
-    arg = fcntl(aFd, F_GETFD);
-    if (-1 == arg)
-        goto Finally;
-
-    arg |= FD_CLOEXEC;
-
-    if (-1 == fcntl(aFd, F_SETFD, arg))
-        goto Finally;
-
-    rc = 0;
-
-Finally:
-
-    return rc;
-}
-
-/*----------------------------------------------------------------------------*/
-int
-fd_close(int aFd)
-{
-    if (-1 != aFd)
-        close(aFd);
-
-    return -1;
-}
-
-/*----------------------------------------------------------------------------*/
-ssize_t
-fd_write(int aFd, const char *aBuf, ssize_t aLen)
-{
-    int rc = -1;
-
-    const char *bufPtr = aBuf;
-    ssize_t     bufLen = aLen;
-
-    while (bufLen) {
-        ssize_t writeLen = write(aFd, bufPtr, bufLen);
-        if (-1 == writeLen) {
-            if (EINTR == errno)
-                continue;
-            if (bufPtr != aBuf)
-                break;
-            goto Finally;
-        }
-
-        if (0 == writeLen)
-            break;
-
-        if (writeLen > bufLen)
-            die("File descriptor %d write %lu overrunning buffer length %lu",
-                aFd, (unsigned long) writeLen, (unsigned long) bufLen);
-
-        bufPtr += writeLen;
-        bufLen -= writeLen;
-    }
-
-    rc = 0;
-
-Finally:
-
-    return rc ? -1 : bufPtr - aBuf;
-}
-
-/*----------------------------------------------------------------------------*/
-ssize_t
-fd_read(int aFd, char *aBuf, ssize_t aLen)
-{
-    int rc = -1;
-
-    char   *bufPtr = aBuf;
-    ssize_t bufLen = aLen;
-
-    while (bufLen) {
-        ssize_t readLen = read(aFd, bufPtr, bufLen);
-        if (-1 == readLen) {
-            if (EINTR == errno)
-                continue;
-            if (bufPtr != aBuf)
-                break;
-            goto Finally;
-        }
-
-        if (0 == readLen)
-            break;
-
-        if (readLen > bufLen)
-            die("File descriptor %d read %lu overrunning buffer length %lu",
-                aFd, (unsigned long) readLen, (unsigned long) bufLen);
-
-        bufPtr += readLen;
-        bufLen -= readLen;
-    }
-
-    rc = 0;
-
-Finally:
-
-    return rc ? -1 : bufPtr - aBuf;
-}
-
-/******************************************************************************/
-struct SignalBlock {
-    sigset_t mSigSet;
-};
-
-static void
-signal_block_acquire(struct SignalBlock *aSignalBlock)
-{
-    sigset_t blockMask;
-
-    if (sigfillset(&blockMask))
-        die("Unable to fill blocking signal set");
-
-    if (sigprocmask(SIG_SETMASK, &blockMask, &aSignalBlock->mSigSet))
-        die("Unable to set blocking signal mask");
-}
-
-
-/*----------------------------------------------------------------------------*/
-static void
-signal_block_release(const struct SignalBlock *aSignalBlock)
-{
-    if (sigprocmask(SIG_SETMASK, &aSignalBlock->mSigSet, 0))
-        die("Unable to reset blocking signal mask");
-}
-
-/******************************************************************************/
-static volatile sig_atomic_t SignalSet_;
-
-/*----------------------------------------------------------------------------*/
-sig_atomic_t
-signal_set_sample(void)
-{
-    struct SignalBlock signalBlock;
-
-    signal_block_acquire(&signalBlock);
-
-    /* Sample the signal set with all signal delivery blocked to
-     * prevent the set being updated while it is being sampled. */
-
-    sig_atomic_t sigSet = SignalSet_;
-    SignalSet_ = 0;
-
-    signal_block_release(&signalBlock);
-
-    return sigSet;
-}
-
-/*----------------------------------------------------------------------------*/
-void
-signal_set_add(int aSignal)
-{
-    struct SignalBlock signalBlock;
-
-    signal_block_acquire(&signalBlock);
-
-    /* Posix says that sig_atomic_t can be signed, so exclude the
-     * sign bit from use in the bitmap. */
-
-    if (aSignal >= sizeof(SignalSet_)*CHAR_BIT - 1)
-        die("Signal %d exceeds set size", aSignal);
-
-    /* Signal numbers are non-zero, but do not bother trying to
-     * recover bit zero from the bitmap. */
-
-    SignalSet_ |= (1 << aSignal);
-
-    signal_block_release(&signalBlock);
-}
-
-/******************************************************************************/
-static struct {
-    const char *mName;
-    int         mSignal;
-    sig_t       mHandler;
-} SigStrategy[] = {
-    { "SIGHUP",  SIGHUP },
-    { "SIGQUIT", SIGQUIT },
-    { "SIGINT",  SIGINT },
-    { "SIGABRT", SIGABRT },
-    { "SIGTERM", SIGTERM },
-    { "SIGCONT", SIGCONT },
-};
-
-/*----------------------------------------------------------------------------*/
-static void
-signal_handler_(int aSignal)
-{
-    signal_set_add(aSignal);
-}
-
-/*----------------------------------------------------------------------------*/
-void
-signal_catch(void)
-{
-    /* Only catch signals that are still set at their default action,
-     * and not being ignored. Processes inherit default and ignored
-     * signal settings from their parent. */
-
-    (void) signal_set_sample();
-
-    for (unsigned ix = 0; ix < NUMBEROF(SigStrategy); ++ix) {
-
-        struct sigaction action;
-
-        if (sigaction(SigStrategy[ix].mSignal, 0, &action))
-            die("Unable to query signal %s", SigStrategy[ix].mName);
-
-        SigStrategy[ix].mHandler = action.sa_handler;
-        action.sa_handler        = signal_handler_;
-
-        if (SIG_DFL == SigStrategy[ix].mHandler) {
-            DEBUG(
-                "Installing catcher for signal %d", SigStrategy[ix].mSignal);
-            if (sigaction(SigStrategy[ix].mSignal, &action, 0))
-                die("Unable to intercept signal %s", SigStrategy[ix].mName);
-        }
-    }
-}
-
-/*----------------------------------------------------------------------------*/
-void
-signal_release(void)
-{
-    for (unsigned ix = 0; ix < NUMBEROF(SigStrategy); ++ix) {
-        struct sigaction action;
-
-        if (sigaction(SigStrategy[ix].mSignal, 0, &action))
-            die("Unable to query signal %s", SigStrategy[ix].mName);
-
-        action.sa_handler = SigStrategy[ix].mHandler;
-
-        if (SIG_DFL == SigStrategy[ix].mHandler) {
-            if (sigaction(SigStrategy[ix].mSignal, &action, 0))
-                die("Unable to reset signal %s", SigStrategy[ix].mName);
-        }
-    }
 }
 
 /******************************************************************************/
@@ -466,7 +68,7 @@ parse_options(int argc, char **argv)
 {
     int rc = -1;
 
-    static char shortOpts[] = "hdfZx:";
+    static char shortOpts[] = "+hdfZx:";
 
     static struct option longOpts[] = {
         { "help",      no_argument,       0, 'h' },
@@ -502,7 +104,7 @@ parse_options(int argc, char **argv)
             optContinue = 1; break;
 
         case 'd':
-            optDebug = 1; break;
+            debug("%s", DebugEnable); break;
 
         case 'x':
             if (!strcmp(optarg, "none")) {
@@ -529,10 +131,8 @@ parse_options(int argc, char **argv)
                     if (!isdigit((unsigned char) word[0]))
                         die("Exit code %s must start with a digit", word);
 
-                    char *endPtr;
-                    unsigned long exitCode = strtoul(word, &endPtr, 10);
-
-                    if (*endPtr)
+                    unsigned long exitCode;
+                    if (int_strtoul(&exitCode, word))
                         die("Unable to parse exit code %s", word);
 
                     if (exitCode > 255)
@@ -560,112 +160,12 @@ Finally:
 }
 
 /******************************************************************************/
-pid_t
-exec_command(char **aCmd)
-{
-    int rc = -1;
-
-    pid_t childPid = -1;
-
-    int pipeRd = -1;
-    int pipeWr = -1;
-
-    {
-        int pipeFds[2];
-        if (pipe(pipeFds)) {
-            error("Unable to create pipe");
-            goto Finally;
-        }
-
-        pipeRd = pipeFds[0];
-        pipeWr = pipeFds[1];
-
-        if (fd_cloexec(pipeRd)) {
-            error("Unable to set FD_CLOEXEC on fd %d", pipeRd);
-            goto Finally;
-        }
-        if (fd_cloexec(pipeWr)) {
-            error("Unable to set FD_CLOEXEC on fd %d", pipeWr);
-            goto Finally;
-        }
-    }
-
-    childPid = fork();
-    if (-1 == childPid) {
-        error("Unable to fork new process");
-        goto Finally;
-    }
-
-    if (!childPid) {
-
-        /* Signals that are being caught will revert to their default
-         * action as described in execve(2). */
-
-        execvp(aCmd[0], aCmd);
-
-        int errCode = errno;
-
-        error("Unable to execute %s", aCmd[0]);
-
-        fd_write(pipeWr, (void *) &errCode, sizeof(errCode));
-
-        _exit(EXIT_FAILURE);
-    }
-
-    DEBUG("Child process %d forked", childPid);
-
-    pipeWr = fd_close(pipeWr);
-
-    int errCode;
-
-    ssize_t readLen = fd_read(pipeRd, (void *) &errCode, sizeof(errCode));
-
-    int execCode = -1;
-
-    if (-1 == readLen)
-        errCode = errno;
-    else if (!readLen)
-        execCode = 0;
-    else if (sizeof(errCode) == readLen)
-        execCode = 1;
-
-    DEBUG("Child process %d exec code %d", childPid, execCode);
-
-    if (execCode) {
-        errno = errCode;
-        goto Finally;
-    }
-
-    rc = 0;
-
-Finally:
-
-    FINALLY({
-        if (rc) {
-            if (-1 != childPid) {
-                kill(childPid, SIGKILL);
-
-                while (childPid != waitpid(childPid, 0, 0))
-                    continue;
-
-                childPid = -1;
-            }
-        }
-
-        pipeRd = fd_close(pipeRd);
-        pipeWr = fd_close(pipeWr);
-    });
-
-    return rc ? -1 : childPid;
-}
-
-/******************************************************************************/
 int
 spawn_command(char **aCmd)
 {
     int rc = -1;
 
-    pid_t childPid = exec_command(aCmd);
+    pid_t childPid = proc_execute(aCmd);
     if (-1 == childPid)
         goto Finally;
 
@@ -678,7 +178,7 @@ spawn_command(char **aCmd)
          * https://people.freebsd.org/~cracauer/homepage-mirror/sigint.html
          */
 
-        sig_atomic_t sigSet = signal_set_sample();
+        sig_atomic_t sigSet = signalset_sample();
 
         for (int signal = 0; sigSet; ++signal) {
             if (sigSet & 1) {
@@ -709,7 +209,7 @@ spawn_command(char **aCmd)
 
             if (SIGSTOP == stopSig || SIGTSTP == stopSig) {
                 if (optContinue) {
-                    signal_set_add(SIGCONT);
+                    signalset_add(SIGCONT);
                     stopSig = 0;
                 }
             }
@@ -757,7 +257,7 @@ respawn_command(char **aCmd)
 
     unsigned backoffWindowSeconds = 0;
 
-    unsigned long windowStartMillis = monomillis();
+    uint64_t windowStartMillis = clk_monomillis();
 
     while (1) {
 
@@ -770,9 +270,9 @@ respawn_command(char **aCmd)
         exitCode = spawn_command(aCmd);
         signal_release();
 
-        unsigned long windowEndMillis = monomillis();
+        uint64_t windowEndMillis = clk_monomillis();
 
-        unsigned long runDurationMillis = windowEndMillis - windowStartMillis;
+        uint64_t runDurationMillis = windowEndMillis - windowStartMillis;
 
         if (-1 == exitCode)
             goto Finally;
@@ -781,7 +281,7 @@ respawn_command(char **aCmd)
          * with EXIT_SUCCESS and did not terminate due to a signal. */
 
         if (!optForever) {
-            if (0 < exitCode && exitCode < NUMBEROF(optExit)) {
+            if (0 <= exitCode && exitCode < NUMBEROF(optExit)) {
                 if (optExit[exitCode])
                     break;
             }
@@ -820,7 +320,7 @@ respawn_command(char **aCmd)
             /* Limit the retries so that a broken program cannot
              * overwhelm the host. */
 
-            usleep(1000);
+            clk_sleepmillis(1);
 
         } else {
 
@@ -837,7 +337,7 @@ respawn_command(char **aCmd)
                 unsigned backoffDelay = rand() % backoffWindowSeconds;
 
                 DEBUG("Waiting %us before respawning", backoffDelay);
-                sleep(backoffDelay);
+                clk_sleepmillis(backoffDelay * 1000);
             }
 
             windowStartMillis = windowEndMillis;
